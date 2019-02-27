bab3e01a7c4d5e69b1cd609ec8cdd16276741afd
Shivendra Saurav <shivendrasaurav@gmail.com>
2019-02-27 10:03:31 +0530

    Add files via upload

--- prim's_algorithm.cpp
+++ prim's_algorithm.cpp
@@ -1,39 +1,78 @@
 #include<stdio.h>
-int ne=1,min,mincost,cost[10][10],visited[10];
-int main(){
-	int n,i,j,a,b,u,v;
-	printf("Enter no of vertices : ");
+ 
+ int a,b,u,v,n,i,j,ne=1;
+ 
+int visited[10]={0},min,mincost=0,cost[10][10];
+ 
+int main()
+ 
+{
+ 
+ 
+	printf("\nEnter the number of vertices:");
+ 
 	scanf("%d",&n);
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++){
-			printf("Enter Cost At (%d,%d)",i+1,j+1);
-			scanf("%d",&cost[i][j]);
-		}
+ 
+	printf("\nEnter the adjacency matrix:\n");
+ 
+	for(i=1;i<=n;i++)
+ 
+	for(j=1;j<=n;j++)
+ 
+	{
+		printf("\nEnter Cost At (%d,%d)",i,j);
+ 
+		scanf("%d",&cost[i][j]);
+ 
+		if(cost[i][j]==0)
+ 
+			cost[i][j]=999;
+ 
 	}
-	if(cost[i][j]==0)
-		cost[i][j]=999;
-	printf("Edges of spanning tree are: \n\n");
+ 
 	visited[1]=1;
-	while(ne<n){
-		for(i=0;min=999,i<n;i++){
-			for(j=0;j<n;j++){
-				if(cost[i][j]<min){
-					if(visited[i]=0)
-						continue;
-					else{
-						min=cost[i][j];
-						a=u=i;
-						b=v=j;
-					}
-				}
-			}
+ 
+	printf("\n");
+ 
+	while(ne < n)
+ 
+	{
+ 
+		for(i=1,min=999;i<=n;i++)
+ 
+		for(j=1;j<=n;j++)
+ 
+		if(cost[i][j]< min)
+ 
+		if(visited[i]!=0)
+ 
+		{
+ 
+			min=cost[i][j];
+ 
+			a=u=i;
+ 
+			b=v=j;
+ 
 		}
-		if(visited[v]==0){
-			printf("\n%d Edge (%d,%d) = %d\n",ne++,a,b,min);
+ 
+		if(visited[u]==0 || visited[v]==0)
+ 
+		{
+ 
+			printf("\n Edge %d:(%d %d) cost:%d",ne++,a,b,min);
+ 
 			mincost+=min;
+ 
 			visited[b]=1;
+ 
 		}
+ 
 		cost[a][b]=cost[b][a]=999;
+ 
 	}
-	printf("\n\nMin Cost : %d",mincost);
+ 
+	printf("\n Minimun cost=%d",mincost);
+ 
+ 
 }
