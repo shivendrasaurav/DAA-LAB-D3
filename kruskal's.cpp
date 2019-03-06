3bac903010c8271a5542d8639ab0fb56bf177d79
Shivendra Saurav <shivendrasaurav@gmail.com>
2019-03-06 11:45:13 +0530

    Update kruskal's.cpp

--- kruskal's.cpp
+++ kruskal's.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int a,b,i,j,k,min,mincost=0,cost[10][10],parent[10]={0},ne=1,n,sorter[100],temp;
+int a,b,u,v,i,j,k,min,mincost=0,cost[10][10],parent[10]={0},ne=1,n,sorter[100],temp;
 
 int main(){
 	printf("Enter Size Of Graph : ");
@@ -29,10 +29,22 @@ int main(){
 			printf("%d, ",cost[i][j]);
 		}
 	}
-	//MATRIX SORTED
+	//MATRIX SORTED]
 	while(ne<n){
 		for(i=0,min=999;i<n;i++)
 			for(j=0;j<n;j++)
-				
+				if(cost[i][j]<min)
+					if(parent[i]==0){
+						min=cost[i][j];
+						a=u=i;
+						b=v=j;
+					}
+					if(parent[u]==0||parent[v]==0){
+						printf("Edge %d(%d,%d) Cost : %d",ne++,a,b,min);
+						mincost+=min;
+						parent[b]=1;
+					}
+				cost[i][j]=cost[j][i]=999;
 	}
+	printf("\nMincost : %d",mincost);
 }
